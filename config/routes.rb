1751189520fa72743d1745ce5617a3019af5363b
Constantine <643711690@qq.com>
2021-02-26 18:00:30 +0800

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get '/contact',to:'static_pages#contact', as: 'contact'
-  get 'help',to:'static_pages#help',as:'help'
-  get('about',{:to=>'static_pages#about',:as=>'about'})
-  get 'signup',to:'users#new'
+  get "/contact", to: "static_pages#contact", as: "contact"
+  get "help", to: "static_pages#help", as: "help"
+  get("about", { :to => "static_pages#about", :as => "about" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root "static_pages#home"
+  get "signup", to: "users#new"
+  resources :users
 end
