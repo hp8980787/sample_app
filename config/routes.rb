eb706266cf024f82accdfc4a2d06600cee6a6476
Constantine <643711690@qq.com>
2021-03-01 14:42:31 +0800

    compelete sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get("about", { :to => "static_pages#about", :as => "about" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "static_pages#home"
-  get "signup", to: "users#new"
+  get "signup", to: "users#new",as:"signup"
   resources :users
 end
